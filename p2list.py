c83e6d2c105f3d0a0058f22057db944219452cce
Dylan Maxwell <maxwelld@frib.msu.edu>
2015-12-07 12:31:04 -0500

    Cleanup scripts and add documentation before committing to main repository.

--- p2list.py
+++ p2list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python2
 
+# NOTE: This script requires location of the Eclipse executable
+# to be specified by the environment variable ECLIPSE_BIN.
+
 import sys, os, os.path, re, time, subprocess
 from StringIO import StringIO
 
+DESCRIPTION="List the Installable Units contained in the specified P2 repository."
 
 if "ECLIPSE_BIN" not in os.environ:
     sys.stderr.write("Eclipse application not found, set ECLIPSE_BIN environment variable\n")
@@ -11,7 +15,7 @@ if "ECLIPSE_BIN" not in os.environ:
 eclipse = os.environ["ECLIPSE_BIN"]
 
 if not os.path.isfile(eclipse):
-    sys.stderr.write("Eclipse application not found, file not found: " + eclipse + "\n");
+    sys.stderr.write("Eclipse application not found, executable not found: " + eclipse + "\n");
     sys.exit(1)
 
 
@@ -29,7 +33,8 @@ def p2list(repo, query=None):
     try:
         output = subprocess.check_output(cmd_args)
     except subprocess.CalledProcessError as e:
-        sys.stderr.write("Error ("  + e.returncode + ")\n")
+        sys.stderr.write("Error (%s)\n" % (e.returncode,))
+        sys.stderr.write(e.output)
         sys.exit(1)
 
     sys.stderr.write("Done (%.2fs)\n" % ((time.time() - start),))
@@ -44,15 +49,13 @@ def p2list(repo, query=None):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 2:
-        ius = p2list(sys.argv[1], sys.argv[2])
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description=DESCRIPTION)
+    parser.add_argument("repository", help="Eclipse P2 repository (http: or file: URL)" )
+    parser.add_argument("query", nargs='?', help="Optional P2 query expression (e.g. 'Q:group')")
 
-    elif len(sys.argv) > 1:
-        ius = p2list(sys.argv[1])
+    args = parser.parse_args(sys.argv[1:])
 
-    else:
-        sys.stderr.write("Usage: p2list.py repoitory [ query ]\n")
-        sys.exit(1)
-
+    ius = p2list(args.repository, args.query)
     for iu in ius:
         sys.stdout.write(iu[0]+'/'+iu[1]+'\n')
--- p2target.py
+++ p2target.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python2
 
+# NOTE: This script requires location of the Eclipse executable
+# to be specified by the environment variable ECLIPSE_BIN.
+
 import sys, p2list
 
 from argparse import FileType
@@ -7,16 +10,31 @@ from argparse import ArgumentParser
 
 from xml.etree import ElementTree
 
-parser = ArgumentParser("")
-parser.add_argument("baseTarget", type=FileType('r'), help="Base target definition")
-parser.add_argument("destTarget", type=FileType('w'), help="Destination target definition", nargs='?', default=sys.stdout)
+DESCRIPTION="""Expand the specified target definition template as follows:
+
+All 'location' elements must contains a valid 'repository' element with a
+'location' attribute. If a 'location' element does NOT contain 'unit'
+elements then the P2 repository is queried for all features using
+the expression 'Q:group'. The 'location' element is then filled with 'unit'
+elements for the latest version of each feature contained in the repository.
+
+If a 'location' element contains one or more 'unit' elements then the P2
+repository is queried for all installable units (IUs). The 'unit' elements
+must specify an 'id' attribute, but the 'version' attribute is optional.
+If the specified IU is found in the repository then the 'version' attribute
+is either completed or verified.
+"""
+
+parser = ArgumentParser(description=DESCRIPTION)
+parser.add_argument("template", type=FileType('r'), help="Location of target definition template")
+parser.add_argument("target", type=FileType('w'), help="Destination of target definition (default: stdout)", nargs='?', default=sys.stdout)
 
 args = parser.parse_args(sys.argv[1:])
 
 
-tree = ElementTree.parse(args.baseTarget)
+tree = ElementTree.parse(args.template)
 if tree is None:
-    sys.stderr.write("Error parsing file: invalid XML file: " + str(baseTarget) + "\n")
+    sys.stderr.write("Error parsing file: invalid XML file: " + str(template) + "\n")
     sys.exit(1)
 
 locations = tree.getroot().find("locations")
@@ -69,12 +87,17 @@ for location in locations.findall("location"):
                         else:
                             elm.set('id', iu[0])
                             elm.set('version', iu[1])
+                        break
+                else:
+                    sys.stderr.write("Error verifying file: IU '%s' not found in repository\n" % (id,))
+                    sys.exit(1)
             else:
                 for iu in ius:
                     if iu[0] == id and iu[1] == ver:
                         break
                 else:
-                    location.remove(unit)
-                    sys.stderr.write("Warning verifying file: " + str(id+'/'+ver) + " not found in repository\n")
-    
-tree.write(args.destTarget, encoding="UTF-8", xml_declaration=True)
+                    sys.stderr.write("Error verifying file: IU '%s' with version '%s' not found in repository\n" % (id,ver))
+                    sys.exit(1)
+
+
+tree.write(args.target, encoding="UTF-8", xml_declaration=True)
